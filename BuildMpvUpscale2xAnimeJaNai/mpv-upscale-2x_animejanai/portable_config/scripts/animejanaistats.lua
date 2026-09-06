c245284ce71132daa0cf2ec3942302f9e498bdc5
sunuuc <77818773+sunuuc@users.noreply.github.com>
2026-09-06 15:31:58 +0800

    Localize AnimeJaNai stats OSD

--- BuildMpvUpscale2xAnimeJaNai/mpv-upscale-2x_animejanai/portable_config/scripts/animejanaistats.lua
+++ BuildMpvUpscale2xAnimeJaNai/mpv-upscale-2x_animejanai/portable_config/scripts/animejanaistats.lua
@@ -3,38 +3,35 @@ local open = io.open
 local showingMessage = false
 local MAX_DURATION = 2147483
 
--- Function to be called when the custom action is triggered
 function show_animejanai_stats()
     if showingMessage then
-        mp.osd_message("")  -- Clear the OSD message
+        mp.osd_message("")
         showingMessage = false
     else
         local message = ""
 
         if mp.get_property("vf") == "" then
-            message = "Upscaling is disabled"
+            message = "超分已关闭"
         else
             local data_file_path = (mp.command_native({'expand-path', "~~/../animejanai/currentanimejanai.log"}))
             message = read_file(data_file_path)
 
             if message == "" then
-                message = "Error during upscale; press ~ to view error in console"
+                message = "超分过程中发生错误；按 ~ 键在控制台查看错误信息"
             end
         end
-        
+
         mp.osd_message(message, MAX_DURATION)
         showingMessage = true
     end
 end
 
 function read_file(path)
-    local file = open(path, "r") -- r read mode and b binary mode
+    local file = open(path, "r")
     if not file then return nil end
-    local content = file:read "*a" -- *a or *all reads the whole file
+    local content = file:read "*a"
     file:close()
     return content
 end
 
-
--- Register the custom action to a key binding (e.g., "Ctrl+Shift+C")
 mp.add_key_binding("Ctrl+j", "show_animejanai_stats", show_animejanai_stats)
